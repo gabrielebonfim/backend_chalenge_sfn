c1c6a8d01f7f1aad8023f8a4cd04391f80fc9d45
Gabriele Bonfim <gabriele_bonfim@outlook.com>
2022-01-28 15:56:16 -0500

    Configurado cron para atualizar os articles

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from cron.tasks import update_articles
 from restore_data.restore_data import RestoreData
 from routes.root import ROOT
 from routes.articles import ARTICLE
@@ -7,22 +8,24 @@ from settings.db import COLLECTIONS
 
 APP = FastAPI()
 
-
 API_ROUTES = [
     APP.include_router(ROOT),
     APP.include_router(ARTICLE)
 ]
 
-
 """
 Restoring the data from Space Flight News API for the first time.
 Restaurando os dados do Space Flight News API pela primeira vez.
 """
-ARTICLES = RestoreData(COLLECTIONS["articles"], 'https://api.spaceflightnewsapi.net/v3/articles').restore()
 
+ARTICLES = RestoreData(COLLECTIONS["articles"], 'https://api.spaceflightnewsapi.net/v3/articles').restore_all()
 
 """
 CRON
 Initizalized at 9:00AM (UTC-3) every day to update data.
 Inicializado às 9:00AM (UTC-3) todos os dias para atualizar os dados.
 """
+
+CRON_JOBS = [
+    update_articles(9)
+]
--- restore_data/restore_data.py
+++ restore_data/restore_data.py
@@ -1,16 +1,31 @@
+from operator import itemgetter
 import requests
 
 
 class RestoreData:
-    def __init__(self, collection, api_url):
+    def __init__(self, collection, api_url, schema=None):
         self.collection = collection
-        self.api_url = api_url
+        self.json = requests.get(api_url).json()
+        self.schema = schema
 
     def __validate_first_import(self):
         count_docs = self.collection.count_documents({})
         return count_docs == 0
 
-    def restore(self):
+    def restore_all(self):
         if self.__validate_first_import():
-            data = requests.get(self.api_url).json()
-            return self.collection.insert_many(data)
+            return self.collection.insert_many(self.json)
+
+    def restore_last(self):
+        json_sorted = sorted(self.json, key=itemgetter('id'))
+
+        def get_json_position():
+            last_original_id = self.schema(self.collection.find().sort("id", -1).limit(1))[0]["id"]
+            for i in range(0, len(json_sorted)):
+                if json_sorted[i]["id"] == last_original_id:
+                    position = i + 1
+                    return position
+
+        new_documents = [json_sorted[i] for i in range(get_json_position(), len(json_sorted))]
+        if new_documents:
+            return self.collection.insert_many(new_documents)
